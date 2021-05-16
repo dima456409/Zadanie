77321e20673fa825db850d5f5fe096b77a0d0154
dima456409 <84269902+dima456409@users.noreply.github.com>
2021-05-16 15:50:02 +0500

    update output message

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -6,7 +6,7 @@ namespace Zadanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ворлд!");
         }
     }
 }
